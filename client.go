ef8699bcdb72766fcff512fa249dc4174055dc6c
ningyixiang <386741589@qq.com>
2020-12-10 17:58:06 +0800

    客户端代码初步完成

---
+++ client.go
@@ -0,0 +1,222 @@
+package golangRpc
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"golangRpc/codec"
+	"io"
+	"log"
+	"net"
+	"sync"
+)
+
+// Call 代表一次有效的rpc调用
+type Call struct {
+	Seq           uint64
+	ServiceMethod string      // 格式： "<service>.<method>"
+	Args          interface{} // 函数所需的参数
+	Reply         interface{} // 函数的返回值
+	Error         error
+	Done          chan *Call //
+}
+
+// 当调用结束时，调用call.done()通知调用方
+func (call *Call) done() {
+	call.Done <- call
+}
+
+// Client代表一次rpc调用的客户端, 一个client可以有多个call
+type Client struct {
+	cc  codec.Codec // 编码解码器，用于序列化和反序列化
+	opt *Option
+
+	sending *sync.Mutex // 保护下列对象的锁
+	header  codec.Header
+
+	mu       *sync.Mutex      // 保护下列对象的锁
+	seq      uint64           // 请求的唯一编号
+	pending  map[uint64]*Call // 存储未处理完的请求
+	closing  bool             // 客户端主动关闭
+	shutdown bool             // 服务端关闭
+}
+
+var _ io.Closer = (*Client)(nil)
+
+var ErrShutdown = errors.New("connection is shut down")
+
+// 关闭连接
+func (client *Client) Close() error {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+
+	if client.closing {
+		return ErrShutdown
+	}
+	client.closing = true
+	return client.cc.Close()
+}
+
+func (client *Client) IsAvailable() bool {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+	return !client.shutdown && !client.closing
+}
+
+// registerCall 将参数 call 添加到 client.pending 中，并更新 client.seq
+func (client *Client) registerCall(call *Call) (uint64, error) {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+	if client.closing || client.shutdown {
+		return 0, ErrShutdown
+	}
+	call.Seq = client.seq
+	client.pending[call.Seq] = call
+	client.seq++
+	return call.Seq, nil
+}
+
+// removeCall 根据 seq，从 client.pending 中移除对应的 call，并返回
+func (client *Client) removeCall(seq uint64) *Call {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+	call := client.pending[seq]
+	delete(client.pending, seq)
+	return call
+}
+
+// terminateCalls 服务端或客户端发生错误时调用，将 shutdown 设置为 true，且将错误信息通知所有 pending 状态的 call
+func (client *Client) terminateCalls(err error) {
+	client.sending.Lock()
+	defer client.sending.Unlock()
+	client.mu.Lock()
+	defer client.mu.Unlock()
+	client.shutdown = true
+	for _, call := range client.pending {
+		call.Error = err
+		call.done()
+	}
+}
+
+// receive 客户端的接受功能
+func (client *Client) receive() {
+	var err error
+	for err == nil {
+		var h codec.Header
+		if err = client.cc.ReadHeader(&h); err != nil {
+			break
+		}
+		// 从pending中取出一个call进行处理
+		call := client.removeCall(h.Seq)
+		// 1. call不存在，可能是请求没有发送完整，或者因为其他原因被取消，但是服务端仍旧处理了
+		// 2. call 存在，但服务端处理出错，即 h.Error 不为空。
+		// 3. call 存在，服务端处理正常，那么需要从 body 中读取 Reply 的值。
+		switch {
+		case call == nil:
+			err = client.cc.ReadBody(nil)
+		case h.Error != "":
+			call.Error = fmt.Errorf(h.Error)
+			err = client.cc.ReadBody(nil)
+		default:
+			err = client.cc.ReadBody(call.Reply)
+			if err != nil {
+				call.Error = errors.New("reading body " + err.Error())
+			}
+			call.done()
+		}
+	}
+	client.terminateCalls(err)
+}
+
+// 创建 Client 实例时，首先需要完成一开始的协议交换，即发送 Option 信息给服务端。
+// 协商好消息的编解码方式之后，再创建一个子协程调用 receive() 接收响应。
+func NewClient(conn net.Conn, opt *Option) (*Client, error) {
+	f := codec.NewCodecFuncMap[opt.CodecType]
+	if f == nil {
+		err := fmt.Errorf("invalid codec type %s", opt.CodecType)
+		log.Println("rpc client: codec error:", err)
+		return nil, err
+	}
+	// send options with server
+	if err := json.NewEncoder(conn).Encode(opt); err != nil {
+		log.Println("rpc client: options error: ", err)
+		_ = conn.Close()
+		return nil, err
+	}
+	return newClientCodec(f(conn), opt), nil
+}
+
+func newClientCodec(cc codec.Codec, opt *Option) *Client {
+	client := &Client{
+		seq:     1, // seq从1开始，0意味着无效的调用
+		cc:      cc,
+		opt:     opt,
+		pending: make(map[uint64]*Call),
+	}
+	go client.receive()
+	return client
+}
+
+// 为了简化用户调用，通过 ...*Option 将 Option 实现为可选参数
+func parseOptions(opts ...*Option) (*Option, error) {
+	if len(opts) == 0 || opts[0] == nil {
+		return DefaultOption, nil
+	}
+	if len(opts) != 1 {
+		return nil, errors.New("number of options is more than 1")
+	}
+
+	opt := opts[0]
+	opt.MagicNumber = DefaultOption.MagicNumber
+	if opt.CodecType == "" {
+		opt.CodecType = DefaultOption.CodecType
+	}
+	return opt, nil
+}
+
+// Dial用于连接rpc server
+func Dial(network, address string, opts ...*Option) (client *Client, err error) {
+	opt, err := parseOptions(opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := net.Dial(network, address)
+	if err != nil {
+		return nil, err
+	}
+	// close the connection if client is nil
+	defer func() {
+		if client == nil {
+			conn.Close()
+		}
+	}()
+	return NewClient(conn, opt)
+}
+
+func (client *Client) send(call *Call) {
+	client.sending.Lock()
+	defer client.sending.Unlock()
+
+	// 注册call
+	seq, err := client.registerCall(call)
+	if err != nil {
+		call.Error = err
+		call.done()
+		return
+	}
+
+	client.header.ServiceMethod = call.ServiceMethod
+	client.header.Seq = seq
+	client.header.Error = ""
+
+	// 编码并发送请求
+	if err := client.cc.Write(&client.header, call.Args); err != nil {
+		call := client.removeCall(seq)
+
+		if call != nil {
+			call.Error = err
+			call.done()
+		}
+	}
+}
